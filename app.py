3f61766ae101ad6f00c8d3f5736d3d16fb45da9c
PiroroWang <97148654+PiroroWang@users.noreply.github.com>
2022-01-06 14:27:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def handle_message(event):
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 27017))
+    port = int(os.environ.get('PORT',0))
     app.run(host='0.0.0.0', port=port)
 if __name__ == "__main__":
     app.run()
